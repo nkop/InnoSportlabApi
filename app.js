a3a43a62a057dc9b52a735652f472959bc1e51e5
nkop <nkop1@avans.nl>
2017-03-26 10:56:58 +0200

    asd

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://admin:admin@innosportlab-shard-00-00-0tqf6.mongodb.net:27017,innosportlab-shard-00-01-0tqf6.mongodb.net:27017,innosportlab-shard-00-02-0tqf6.mongodb.net:27017/InnoSportlab?ssl=true&replicaSet=InnoSportlab-shard-0&authSource=admin');
 mongoose.Promise = require('q').Promise;
 
+
 //models
 require('./config/passport')(passport);
 require('./models/user');
