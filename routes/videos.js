7b654d8f3ccd3a94c50a995737d74111b97f4287
nkop <nkop1@avans.nl>
2017-06-11 16:00:08 +0200

    hmm

--- routes/videos.js
+++ routes/videos.js
@@ -72,7 +72,7 @@ var storage = GridFsStorage({
     metadata: function(req, file, cb) {
         cb(null,
             {   originalname: file.originalname,
-                videoId: req.params.username
+                videoId: req.params.id
             });
     },
     root: 'ctFiles' //root name for collection to store files into
@@ -85,7 +85,7 @@ var upload = multer({ //multer settings for single upload
 function getVideo(req, res){
     gfs.collection('ctFiles'); //set collection name to lookup into
     Video.findOne({'_id': req.params.id }, function(err, video) {
-        gfs.files.find({'filename': video._id }).toArray(function (err, files) {
+        gfs.files.find({'metadata.videoId': video._id }).toArray(function (err, files) {
             if (!files || files.length === 0) {
                 return res.status(404).json({
                     responseCode: 1,
