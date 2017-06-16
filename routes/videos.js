ff4f056aecc74509fa865c2745716491a05bad99
Jeroentje61 <jfpbreke@avans.nl>
2017-06-16 16:04:00 +0200

    testheroku

--- routes/videos.js
+++ routes/videos.js
@@ -26,7 +26,7 @@ var vid;
 
 router.use(function(req, res, next) { //allow cross origin requests
     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", true);
     next();
